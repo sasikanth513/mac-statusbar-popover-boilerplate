bdcb3800d45ebebaf934a3628e6519541402629a
sasikanth513 <dronavalli.sasi@gmail.com>
2018-07-14 20:08:38 +0530

    Initial Commit

---
+++ ClockV1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ClockV1
+//
+//  Created by Sasikanth Dronavalli on 14/07/18.
+//  Copyright © 2018 Sasikanth Dronavalli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ClockV1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClockV1
+//
+//  Created by Sasikanth Dronavalli on 14/07/18.
+//  Copyright © 2018 Sasikanth Dronavalli. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
